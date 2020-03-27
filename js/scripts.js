3c7793ad304399fab812400ba4a83f191e5e3c72
shonburgundy <seanbeloussow@yahoo.com>
2020-03-27 14:04:45 -0700

    concatenated name variable into .textfunction, added user name input

--- js/scripts.js
+++ js/scripts.js
@@ -19,18 +19,21 @@ $(document).ready(function() {
     var experience = $("#exp-input").val();
     var prefer = $("#pref-input").val();
     var holiday = $("#holiday-input").val();
-    console.log(animal, meal, experience, prefer, holiday)
+    var name = $("#user-name").val();
+    //console.log(animal, meal, experience, prefer, holiday)
     var result;
-
+    if (! name) {
+      name="Hey Mystery Person"
+    }
     if (animal === "Dogs" && meal === "Steak & Eggs" && experience === "None" &&  prefer === "Mac" && holiday === "Batman Day") {
-      result = "You should look into Ruby!"
+      result = "you should look into Ruby!"
     } else if (animal === "Cats" && meal === "Beet Salad" && experience === "A little" &&  prefer === "Lenovo" && holiday === "National Soft Ice Cream Day") {
-      result = "You should look into Python!"
+      result = "you should look into Python!"
     } else if (animal === "Horses" && meal === "Chicken Tendies" && experience === "A plethora" &&  prefer === "Dell" && holiday === "Christmas") {
-      result = "You should look into C#"
+      result = "you should look into C#"
     }
 
-    $("#output").text(result);
+    $("#output").text(name + ", " + result);
     $("#output").show();
   });
 });  
