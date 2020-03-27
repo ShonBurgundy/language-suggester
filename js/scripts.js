b5ea118a3ca49c61df50176d1c21e6a26e8798b0
shonburgundy <seanbeloussow@yahoo.com>
2020-03-27 10:58:13 -0700

    added else if statments

--- js/scripts.js
+++ js/scripts.js
@@ -13,16 +13,23 @@
 //Front-End (User interface logic)
 
 $(document).ready(function() {
-  $("#submitbutton").submit(function() {
+  $("#submitbutton").click(function() {
     var animal = $("animal-input").val();
     var meal = $("meal-input").val();
     var experience = $("exp-input").val();
     var prefer = $("pref-input").val();
     var holiday = $("holiday-input").val();
+    console.log(animal, meal, experience, prefer, holiday)
     var result;
 
-    if (animal === "Dogs" && meal === "Steak & Eggs" && experience === "None" &&  prefer === "Mac" && holiday === "Batman Day")
+    if (animal === "Dogs" && meal === "Steak & Eggs" && experience === "None" &&  prefer === "Mac" && holiday === "Batman Day") {
+      result = "You should look into Ruby!"
+    } else if (animal === "Cats" && meal === "Beet Salad" && experience === "A little" &&  prefer === "Lenovo" && holiday === "National Soft Ice Cream Day") {
+      result = "You should look into Python!"
+    } else if (animal === "Horses" && meal === "Chicken Tendies" && experience === "A plethora" &&  prefer === "Dell" && holiday === "Christmas") {
+      result = "You should look into C#"
+    }
 
-
+    $("#output").text(result);
   });
 });  
