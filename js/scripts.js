4afd68cc4371ffe30007bed4f4001b2806503ac9
shonburgundy <seanbeloussow@yahoo.com>
2020-03-27 09:51:11 -0700

    defined variables in js, indentation fix in html

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+//Back-End (Business Logic)
+
+var animal = $("animal-input").val();
+
+var meal = $("meal-input").val();
+
+var experience = $("exp-input").val();
+
+var prefer = $("pref-input").val();
+
+var holiday = $("holiday-input").val();
+
+
+
+
+
+
+
+
+
+
+//Front-End (User interface logic)
+
+$(document).ready(function() {
+
+
+  });
+});  
