34543668db010835e40cc82df9d6277b7c4d569c
shonburgundy <seanbeloussow@yahoo.com>
2020-03-27 10:12:13 -0700

    added divs to html, front end JS started

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,16 @@ var holiday = $("holiday-input").val();
 //Front-End (User interface logic)
 
 $(document).ready(function() {
+  $("#submitbutton").submit(function() {
+    var animal = $("animal-input").val();
+
+    var meal = $("meal-input").val();
+
+    var experience = $("exp-input").val();
+
+    var prefer = $("pref-input").val();
+
+    var holiday = $("holiday-input").val();
 
 
   });
